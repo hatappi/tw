ed02e664b668b71cfb430d2c8a9c9278d1ffac06
hatappi <hatappi@hatappi.me>
2019-10-12 11:50:34 +0900

    feat: edit message for tweet by editor

---
+++ editor/editor.go
@@ -0,0 +1,35 @@
+package editor
+
+import (
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+func EditText() ([]byte, error) {
+	tmpfile, err := ioutil.TempFile("", "tw")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	if err := openEditor(tmpfile.Name()); err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(tmpfile.Name())
+}
+
+func openEditor(filepath string) error {
+	cmdName := "vi"
+	if e := os.Getenv("EDITOR"); e != "" {
+		cmdName = e
+	}
+
+	c := exec.Command(cmdName, filepath)
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	return c.Run()
+}
